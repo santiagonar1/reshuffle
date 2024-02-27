29ccc818b22d7c156d946d72f7f1f84c83efaf11
Santiago Narvaez <santiagonar1@gmail.com>
2024-02-27 12:58:05 +0100

    feat: add dummy mpi test

---
+++ tests/mpi_tests/dummy_mpi_test.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <mpi.h>
+
+using ::testing::Ge;
+
+TEST(DummyMPITest, TestingMPIEnvironmentWorks) {
+    int rank{};
+    int size{};
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    EXPECT_THAT(size, Ge(2));
+
+    if (rank == 0) {
+        MPI_Send(&rank, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    } else if (rank == 1) {
+        MPI_Recv(&rank, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+    }
+
+    EXPECT_THAT(rank, Ge(0));
+}
