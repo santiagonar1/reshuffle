b0a2892173631a7fe88b4eb2a02fe4f671b8e29f
Santiago Narvaez <santiagonar1@gmail.com>
2024-02-28 11:34:05 +0100

    feat: make shuffle work with different datatypes

--- include/reshuffle.hpp
+++ include/reshuffle.hpp
@@ -5,15 +5,34 @@
 #include <mpi.h>
 
 namespace reshuffle {
-    std::vector<int> shuffle(const std::vector<int> &values, const MPI_Comm &comm) {
+
+    namespace details {
+        template<typename DATATYPE>
+        MPI_Datatype to_mpi_datatype() {
+            if (std::is_same_v<DATATYPE, int>) {
+                return MPI_INT;
+            } else if (std::is_same_v<DATATYPE, float>) {
+                return MPI_FLOAT;
+            } else if (std::is_same_v<DATATYPE, double>) {
+                return MPI_DOUBLE;
+            }
+
+            throw std::invalid_argument("No MPI Datatype");
+        }
+    }
+
+    template<typename T>
+    auto shuffle(const std::vector<T> &values, const MPI_Comm &comm) {
         int num_ranks{};
         MPI_Comm_size(comm, &num_ranks);
 
         int values_per_rank = static_cast<int>(values.size()) / num_ranks;
         MPI_Bcast(&values_per_rank, 1, MPI_INT, 0, comm);
 
-        std::vector<int> my_values(values_per_rank);
-        MPI_Scatter(values.data(), values_per_rank, MPI_INT, my_values.data(), my_values.size(), MPI_INT, 0, comm);
+        std::vector<T> my_values(values_per_rank);
+        MPI_Scatter(values.data(), values_per_rank, details::to_mpi_datatype<T>(), my_values.data(), my_values.size(),
+                    details::to_mpi_datatype<T>(),
+                    0, comm);
 
         return my_values;
     }
--- tests/mpi_tests/shuffle_test.cpp
+++ tests/mpi_tests/shuffle_test.cpp
@@ -24,3 +24,19 @@ TEST(Shuffle, SplitsDataEquallyAmongRanks) {
     EXPECT_THAT(my_values.size(), Eq(expected_elements_per_rank));
 }
 
+TEST(Shuffle, WorksForDifferentDatatypes) {
+    int rank{};
+    int num_ranks{};
+    constexpr int expected_elements_per_rank = 10;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_ranks);
+
+    std::vector<double> global_values_d{};
+    if (rank == 0) {
+        global_values_d.resize(num_ranks * expected_elements_per_rank);
+    }
+
+    auto my_values_d = reshuffle::shuffle(global_values_d, MPI_COMM_WORLD);
+    EXPECT_THAT(expected_elements_per_rank, Eq(my_values_d.size()));
+}
