bea7fff42318769329a1283ac9a167df55d41b67
Santiago Narvaez <santiagonar1@gmail.com>
2024-02-28 13:11:33 +0100

    refactor: create fixture to group MPI data

--- tests/mpi_tests/shuffle_test.cpp
+++ tests/mpi_tests/shuffle_test.cpp
@@ -7,36 +7,38 @@
 
 using ::testing::Eq;
 
-TEST(Shuffle, SplitsDataEquallyAmongRanks) {
-    int rank{};
-    int num_ranks{};
-    constexpr int expected_elements_per_rank = 10;
+class Shuffle : public testing::Test {
+protected:
+    int _num_ranks{};
+    int _rank{};
+    const int _min_elements_per_rank{10};
 
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &num_ranks);
+    Shuffle() {
+        MPI_Comm_rank(MPI_COMM_WORLD, &_rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &_num_ranks);
+    }
 
+    [[nodiscard]] bool is_root() const {
+        return _rank == 0;
+    }
+};
+
+TEST_F(Shuffle, SplitsDataEquallyAmongRanks) {
     std::vector<int> global_values{};
-    if (rank == 0) {
-        global_values.resize(num_ranks * expected_elements_per_rank);
+    if (is_root()) {
+        global_values.resize(_num_ranks * _min_elements_per_rank);
     }
 
     auto my_values = reshuffle::shuffle(global_values, MPI_COMM_WORLD);
-    EXPECT_THAT(my_values.size(), Eq(expected_elements_per_rank));
+    EXPECT_THAT(my_values.size(), Eq(_min_elements_per_rank));
 }
 
-TEST(Shuffle, WorksForDifferentDatatypes) {
-    int rank{};
-    int num_ranks{};
-    constexpr int expected_elements_per_rank = 10;
-
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &num_ranks);
-
-    std::vector<double> global_values_d{};
-    if (rank == 0) {
-        global_values_d.resize(num_ranks * expected_elements_per_rank);
+TEST_F(Shuffle, WorksForDifferentDatatypes) {
+    std::vector<double> global_values{};
+    if (is_root()) {
+        global_values.resize(_num_ranks * _min_elements_per_rank);
     }
 
-    auto my_values_d = reshuffle::shuffle(global_values_d, MPI_COMM_WORLD);
-    EXPECT_THAT(expected_elements_per_rank, Eq(my_values_d.size()));
+    auto my_values_d = reshuffle::shuffle(global_values, MPI_COMM_WORLD);
+    EXPECT_THAT(_min_elements_per_rank, Eq(my_values_d.size()));
 }
