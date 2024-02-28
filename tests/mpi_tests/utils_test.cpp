e42490e1b1e2b4cc43e196e4f1368f3e49365c7e
Santiago Narvaez <santiagonar1@gmail.com>
2024-02-28 13:59:58 +0100

    test: add testing for to_mpi_datatype

---
+++ tests/mpi_tests/utils_test.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <mpi.h>
+#include <utils.hpp>
+
+using ::testing::Eq;
+using reshuffle::details::to_mpi_datatype;
+
+TEST(ToMPIDatatype, ConvertsDatatypeToMPIDatatype) {
+    EXPECT_THAT(to_mpi_datatype<int>(), Eq(MPI_INT));
+    EXPECT_THAT(to_mpi_datatype<float>(), Eq(MPI_FLOAT));
+    EXPECT_THAT(to_mpi_datatype<double>(), Eq(MPI_DOUBLE));
+}
+
+TEST(ToMPIDatatype, ThrowsIfDatatypeCannotBeConverted) {
+    EXPECT_THROW(to_mpi_datatype<char>(), std::invalid_argument);
+}
