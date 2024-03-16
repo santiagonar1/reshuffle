c8033a21be24d584c6421a69fc1f26f8835a600e
Santiago Narvaez <santiagonar1@gmail.com>
2024-03-16 10:21:20 +0100

    feat: add constraints to Serializable
    We need a way to guarantee that we will be able to create an object of
    the type shuffle is being called with. This can be a problem for non-aggregate
    types, as they might not have a default constructor. So for now, we require that any
    non-trivially serializable object implements a create method. In principle this is
    only required if the non-aggregate type is also not default constructible.

--- include/concepts.hpp
+++ include/concepts.hpp
@@ -5,8 +5,19 @@
 
 namespace reshuffle {
     template<typename T>
-    concept Serializable = zpp::bits::concepts::byte_serializable < T> ||
-    zpp::bits::concepts::has_serialize<T>;
+    concept DefaultConstructible = std::is_default_constructible_v<T>;
+
+    template<typename T>
+    concept TriviallySerializable = zpp::bits::concepts::byte_serializable<T>;
+
+    template<typename T>
+    concept NonTriviallySerializable = zpp::bits::concepts::has_serialize<T> && requires(T &t) {
+        // TODO: We only require this if class is not default_constructible. Add a constraint later on if needed
+        { T::create() } -> std::same_as<T>;
+    };
+
+    template<typename T>
+    concept Serializable = TriviallySerializable<T> || NonTriviallySerializable<T>;
 
     template<typename T>
     concept Iterable = std::ranges::range<T>;
