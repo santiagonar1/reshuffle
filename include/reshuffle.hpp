744abedb7194d6226b6141137cda8ba7b42eea9b
Santiago Narvaez <santiagonar1@gmail.com>
2024-04-25 10:36:30 +0200

    refactor: simplify include to only require reshuffle
    Before we needed to include two separate headers, one for coloring
    functionalities, and other for shuffling. With this commit the user of the library
    only need now to include reshuffle, and he/she will have access
    to both things.

--- include/reshuffle.hpp
+++ include/reshuffle.hpp
@@ -1,74 +1,7 @@
-#ifndef RESHUFFLE_SHUFFLE_HPP
-#define RESHUFFLE_SHUFFLE_HPP
+#ifndef RESHUFFLE_RESHUFFLE_HPP
+#define RESHUFFLE_RESHUFFLE_HPP
 
-#include <vector>
-#include <mpi.h>
-#include <numeric>
-#include <algorithm>
-#include <ranges>
+#include "shuffle.hpp"
+#include "coloring.hpp"
 
-#include "mpi_utils.hpp"
-#include "concepts.hpp"
-
-namespace reshuffle {
-    template<ContiguousContainer C>
-    requires Serializable<typename C::value_type>
-    auto shuffle(const C &values, const MPI_Comm &comm, const std::vector<int> &coloring = {}) {
-        const auto using_coloring = not coloring.empty();
-        if (using_coloring and coloring.size() != std::ranges::size(values)) {
-            throw std::invalid_argument("Coloring being used, but size of coloring vector does not match size of data");
-        }
-
-        const auto all_coloring = internal::gather_values_in_root(coloring, comm);
-        return internal::scatter_values_from_root(internal::gather_values_in_root(values, comm), comm, all_coloring);
-    }
-
-    template<ContiguousContainer C>
-    requires Serializable<typename C::value_type>
-    auto shuffle(const C &values, const MPI_Comm &origin_comm, const MPI_Comm &destiny_comm,
-                 const std::vector<int> &coloring = {}) {
-        using T = C::value_type;
-        if (not internal::mpi_comm_contains_root(origin_comm) or not internal::mpi_comm_contains_root(destiny_comm)) {
-            throw std::invalid_argument("The root process must be included in both origin_comm and destiny_comm");
-        }
-
-        const auto using_coloring = not coloring.empty();
-        if (using_coloring and coloring.size() != std::ranges::size(values)) {
-            throw std::invalid_argument("Coloring being used, but size of coloring vector does not match size of data");
-        }
-
-        auto all_coloring = std::vector<int>{};
-        auto all_values = std::vector<T>{};
-
-        if (internal::in_mpi_comm(origin_comm)) {
-            all_coloring = internal::gather_values_in_root(coloring, origin_comm);
-            all_values = internal::gather_values_in_root(values, origin_comm);
-        }
-
-        const auto my_values = internal::in_mpi_comm(destiny_comm) ? internal::scatter_values_from_root(all_values,
-                                                                                                        destiny_comm,
-                                                                                                        all_coloring)
-                                                                   : std::vector<T>{};
-
-        return my_values;
-    }
-
-    template<Iterable I>
-    requires Serializable<typename I::value_type>
-    auto shuffle(const I &values, const MPI_Comm &comm, const std::vector<int> &coloring = {}) {
-        using T = I::value_type;
-        const std::vector<T> v_values(std::ranges::begin(values), std::ranges::end(values));
-        return shuffle(v_values, comm, coloring);
-    }
-
-    template<Iterable I>
-    requires Serializable<typename I::value_type>
-    auto shuffle(const I &values, const MPI_Comm &origin_comm, const MPI_Comm &destiny_comm,
-                 const std::vector<int> &coloring = {}) {
-        using T = I::value_type;
-        const std::vector<T> v_values(std::ranges::begin(values), std::ranges::end(values));
-        return shuffle(v_values, origin_comm, destiny_comm, coloring);
-    }
-}
-
-#endif //RESHUFFLE_SHUFFLE_HPP
+#endif //RESHUFFLE_RESHUFFLE_HPP
--- tests/serial_tests/coloring_test.cpp
+++ tests/serial_tests/coloring_test.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-#include <coloring.hpp>
+#include <reshuffle.hpp>
 
 using ::testing::Eq;
 
