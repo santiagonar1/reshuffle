31a13196001cd4e8292c5cf0b64e5b804c13b704
Santiago Narvaez <santiagonar1@gmail.com>
2024-04-26 18:28:46 +0200

    refactor: use subdomains instead of coloring descriptor
    We were defining this weird ColoringDescriptor type, which was simply a
    vector of subdomains. This was causing some confusion, as these types of vector
    were sometimes also called coloring. Since I just want to refer as coloring vectors of
    rank ids, I have removed this datatype.

--- include/coloring.hpp
+++ include/coloring.hpp
@@ -9,10 +9,9 @@
 #include "indices.hpp"
 #include "left_closed_range.hpp"
 #include "rank_id.hpp"
+#include "subdomain.hpp"
 
 namespace reshuffle {
-    using ColoringDescriptor = std::vector<internal::LeftClosedRange>;
-
     class BlockWise {
     private:
         const int _num_blocks;
@@ -24,37 +23,37 @@ namespace reshuffle {
     public:
         explicit BlockWise(int num_blocks) : _num_blocks(num_blocks) {}
 
-        [[nodiscard]] ColoringDescriptor get_coloring_descriptor(int num_values) const {
+        [[nodiscard]] auto get_subdomains(int num_values) const {
             const auto min_values_per_block = get_min_values_per_block(num_values);
-            ColoringDescriptor coloring_descriptor{};
+            std::vector<internal::Subdomain> subdomains{};
 
             for (int i = 0; i < min_values_per_block * _num_blocks; i += min_values_per_block) {
                 const auto starting_index = i;
                 const auto last_index = starting_index + min_values_per_block;
-                coloring_descriptor.emplace_back(starting_index, last_index);
+                subdomains.emplace_back(starting_index, last_index);
             }
 
-            coloring_descriptor.back().second = num_values;
-            return coloring_descriptor;
+            subdomains.back().second = num_values;
+            return subdomains;
         }
     };
 
     namespace internal {
-        rank_id get_color(const ColoringDescriptor &coloring_descriptor, int i) {
-            auto it = std::ranges::find_if(coloring_descriptor, [i](const auto &r) { return in_range(r, i); });
+        rank_id get_color(const std::vector<Subdomain> &subdomains, int i) {
+            auto it = std::ranges::find_if(subdomains, [i](const auto &subdomain) { return in_range(subdomain, i); });
             //TODO: Should we check whether the index requested is out of bounds?
-            return static_cast<int>(std::distance(coloring_descriptor.begin(), it));
+            return static_cast<int>(std::distance(subdomains.begin(), it));
         }
 
         Indices2D to_2D(int num_columns, int index) {
             return {index % num_columns, index / num_columns};
         }
 
-        auto get_subdomains(const Dimensions2D &global_dimensions,
-                            const std::array<BlockWise, 2> &strategies) {
-            const auto coloring_x = strategies[0].get_coloring_descriptor(global_dimensions.num_columns);
-            const auto coloring_y = strategies[1].get_coloring_descriptor(global_dimensions.num_rows);
-            return internal::combine(coloring_x, coloring_y);
+        auto get_subdomains_2D(const Dimensions2D &global_dimensions,
+                               const std::array<BlockWise, 2> &strategies) {
+            const auto subdomains_x = strategies[0].get_subdomains(global_dimensions.num_columns);
+            const auto subdomains_y = strategies[1].get_subdomains(global_dimensions.num_rows);
+            return internal::combine(subdomains_x, subdomains_y);
         }
 
     }
@@ -67,12 +66,12 @@ namespace reshuffle {
     auto create_coloring(const std::vector<rank_id> &global_coloring,
                          const BlockWise &strategy, rank_id rank) {
         const auto num_values = static_cast<int>(global_coloring.size());
-        const auto coloring_descriptor = strategy.get_coloring_descriptor(num_values);
+        const auto subdomains = strategy.get_subdomains(num_values);
 
         auto new_global_coloring = std::vector<rank_id>(global_coloring.size());
         auto local_coloring = std::vector<rank_id>{};
         for (int i = 0; i < global_coloring.size(); ++i) {
-            new_global_coloring[i] = internal::get_color(coloring_descriptor, i);
+            new_global_coloring[i] = internal::get_color(subdomains, i);
             if (rank == global_coloring[i]) {
                 local_coloring.push_back(new_global_coloring[i]);
             }
@@ -84,7 +83,7 @@ namespace reshuffle {
     auto create_coloring(const std::vector<rank_id> &global_coloring,
                          const Dimensions2D &global_dimensions,
                          const std::array<BlockWise, 2> &strategies, rank_id rank) {
-        const auto subdomains = internal::get_subdomains(global_dimensions, strategies);
+        const auto subdomains = internal::get_subdomains_2D(global_dimensions, strategies);
 
         auto new_global_coloring = std::vector<rank_id>(global_coloring.size());
         auto local_coloring = std::vector<rank_id>{};
@@ -104,7 +103,7 @@ namespace reshuffle {
     }
 
     auto get_subdomain_dimension(const BlockWise &strategy, int num_values, rank_id rank) {
-        const auto coloring_descriptor = strategy.get_coloring_descriptor(num_values);
+        const auto coloring_descriptor = strategy.get_subdomains(num_values);
 
         if (rank >= coloring_descriptor.size()) {
             return 0;
@@ -115,7 +114,7 @@ namespace reshuffle {
 
     auto get_subdomain_dimension(const std::array<BlockWise, 2> &strategies, Dimensions2D global_dimensions,
                                  rank_id rank) {
-        const auto subdomains = internal::get_subdomains(global_dimensions, strategies);
+        const auto subdomains = internal::get_subdomains_2D(global_dimensions, strategies);
 
         if (rank >= subdomains.size()) {
             return Dimensions2D{0, 0};
---
+++ include/subdomain.hpp
@@ -0,0 +1,10 @@
+#ifndef RESHUFFLE_SUBDOMAIN_HPP
+#define RESHUFFLE_SUBDOMAIN_HPP
+
+#include "left_closed_range.hpp"
+
+namespace reshuffle::internal {
+    using Subdomain = internal::LeftClosedRange;
+}
+
+#endif //RESHUFFLE_SUBDOMAIN_HPP
