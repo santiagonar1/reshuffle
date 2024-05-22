ee7b48ac516b91cf94666e1d261cbc25193970fd
Santiago Narvaez <santiagonar1@gmail.com>
2024-05-22 16:39:09 +0200

    feat: add working example of reduction
    As with expansion, it seems that doing more than
    one results in errors, but at least the first one
    works for now.

--- apps/dyn_proc_demo.cpp
+++ apps/dyn_proc_demo.cpp
@@ -47,6 +47,7 @@ std::string get_grown_main_pset(std::string default_pset, const MPI_Session &ses
 std::string get_main_pset(const MPI_Session &session);
 
 void request_expansion(const std::string &main_pset, const MPI_Session &session);
+void request_reduction(const std::string &main_pset, const MPI_Session &session);
 
 std::pair<int, StringArray> get_set_operation_info(std::string main_pset,
                                                    const MPI_Session &session);
@@ -54,6 +55,18 @@ std::pair<int, StringArray> get_set_operation_info(std::string main_pset,
 std::string get_new_main_pset(std::string main_pset, std::string delta_pset,
                               const MPI_Session &session);
 
+bool is_process_leaving(std::string delta_pset, const MPI_Session &session) {
+    MPI_Info info = MPI_INFO_NULL;
+    auto boolean_string = std::string{16, ' '};
+    int flag{};
+
+    /* Is proc included in the delta PSet? If yes, need to terminate */
+    MPI_Session_get_pset_info(session, delta_pset.data(), &info);
+    MPI_Info_get(info, "mpi_included", 6, boolean_string.data(), &flag);
+    MPI_Info_free(&info);
+
+    return strcmp(boolean_string.data(), "True") == 0;
+}
 
 int main() {
     MPI_Session session = MPI_SESSION_NULL;
@@ -67,13 +80,13 @@ int main() {
     auto num_ranks = get_num_ranks(comm);
 
     const auto dyn_proc = is_dynamic_process(session);
+    auto data = dyn_proc ? std::vector<int>{} : std::vector<int>(20, 42);
 
     constexpr int num_adaptations = 1;
     int starting_iteration{};
 
     if (dyn_proc) {
         MPI_Bcast(&starting_iteration, 1, MPI_INT, 0, comm);
-        auto data = std::vector<int>{};
         data = reshuffle::shuffle(data, comm);
         std::cout << "Dynamic: After adaptation " << rank << " of " << num_ranks << " with "
                   << data.size() << " values\n";
@@ -83,32 +96,39 @@ int main() {
         std::cout << "Origin: Hello from rank " << rank << " of " << num_ranks << std::endl;
 
         /* One process needs to request the set operation and publish the kickof information */
-        if (rank == 0) { request_expansion(main_pset, session); }
+        if (rank == 0) { request_reduction(main_pset, session); }
 
         /* All processes can query the information about the pending Set operation */
         auto [op, output_psets] = get_set_operation_info(main_pset, session);
 
+        main_pset = get_new_main_pset(main_pset, output_psets._data[0], session);
+
+        MPI_Comm old_comm = comm;
+
+        if (is_process_leaving(output_psets._data[0], session)) {
+            data = reshuffle::shuffle(data, old_comm, MPI_COMM_NULL);
+            break;
+        }
+
+        comm = get_comm_from_pset(main_pset, session);
+        rank = get_rank(comm);
+        num_ranks = get_num_ranks(comm);
+
         if (op == MPI_PSETOP_GROW) {
-            main_pset = get_new_main_pset(main_pset, output_psets._data[0], session);
-
-            MPI_Comm old_comm = comm;
-            comm = get_comm_from_pset(main_pset, session);
-            rank = get_rank(comm);
-            num_ranks = get_num_ranks(comm);
-
-            std::cout << "Origin: After adaptation " << rank << " of " << num_ranks << std::endl;
-
             int next_iter = i + 1;
             MPI_Bcast(&next_iter, 1, MPI_INT, 0, comm);
-
-            auto data = std::vector<int>(20, 42);
             data = reshuffle::shuffle(data, comm);
-
-            MPI_Comm_disconnect(&old_comm);
-
-            /* Indicate completion of the Pset operation*/
-            if (rank == 0) { MPI_Session_dyn_finalize_psetop(session, main_pset.data()); }
+        } else if (op == MPI_PSETOP_SHRINK) {
+            data = reshuffle::shuffle(data, old_comm, comm);
         }
+
+        std::cout << "Origin: After adaptation " << rank << " of " << num_ranks << " with "
+                  << data.size() << " values\n";
+
+        MPI_Comm_disconnect(&old_comm);
+
+        /* Indicate completion of the Pset operation*/
+        if (rank == 0) { MPI_Session_dyn_finalize_psetop(session, main_pset.data()); }
     }
 
     /* Disconnect from the old communicator */
@@ -118,6 +138,33 @@ int main() {
     MPI_Session_finalize(&session);
 
     return 0;
+}
+
+void request_reduction(const std::string &main_pset, const MPI_Session &session) {
+    int op = MPI_PSETOP_SHRINK;
+    const int num_shrink_proc = 2;
+
+    MPI_Info info = MPI_INFO_NULL;
+    MPI_Info_create(&info);
+    MPI_Info_set(info, "mpi_num_procs_sub", std::to_string(num_shrink_proc).data());
+
+    StringArray input_psets(1);
+    StringArray output_psets;
+    input_psets._data[0] = strdup(main_pset.data());
+
+    int noutput{};
+
+    /* Send the Set Operation request */
+    MPI_Session_dyn_v2a_psetop(session, &op, input_psets._data, 1, &output_psets._data, &noutput,
+                               info);
+    output_psets._size = noutput;
+    MPI_Info_free(&info);
+
+    /* Publish the name of the new main PSet on the delta Pset */
+    MPI_Info_create(&info);
+    MPI_Info_set(info, "main_pset", output_psets._data[1]);
+    MPI_Session_set_pset_data(session, output_psets._data[0], info);
+    MPI_Info_free(&info);
 }
 
 void request_expansion(const std::string &main_pset, const MPI_Session &session) {
