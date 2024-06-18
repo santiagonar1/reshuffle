7b014bef3efe4d21e1056599470e3fc1de908cfe
Santiago Narvaez <santiagonar1@gmail.com>
2024-06-18 15:58:10 +0200

    test: add test for Block
    The test will also make explicit that a Block is in our case
    a left closed interval.

---
+++ tests/serial_tests/block_test.cpp
@@ -0,0 +1,16 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <block.hpp>
+
+using ::testing::Eq;
+
+TEST(Block, IsALeftClosedInterval) {
+    constexpr int left_bound{0};
+    constexpr int right_bound{4};
+
+    const auto block = reshuffle::Block{left_bound, right_bound};
+
+    EXPECT_TRUE(block.contains(left_bound));
+    EXPECT_FALSE(block.contains(right_bound));
+}
