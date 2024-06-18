fa243ecfdf8de58436e5f6d000af9f8c3d86cd27
Santiago Narvaez <santiagonar1@gmail.com>
2024-06-18 15:49:08 +0200

    test: check that BlockWise adds remaining elements to last block

--- tests/serial_tests/data_distribution_test.cpp
+++ tests/serial_tests/data_distribution_test.cpp
@@ -12,8 +12,14 @@ TEST(BlockWise, SplitsADomainInEqualBlocksIfEvenlyDivisible) {
     const auto block_data_distribution = reshuffle::BlockWise(2);
     const auto subdomains = block_data_distribution.get_blocks(10);
 
-    EXPECT_THAT(subdomains,
-                Eq(std::vector{reshuffle::Block{0, 5}, reshuffle::Block{5, 10}}));
+    EXPECT_THAT(subdomains, Eq(std::vector{reshuffle::Block{0, 5}, reshuffle::Block{5, 10}}));
+}
+
+TEST(BlockWise, AddsRemainingElementsToLastBlock) {
+    const auto block_data_distribution = reshuffle::BlockWise(2);
+    const auto subdomains = block_data_distribution.get_blocks(11);
+
+    EXPECT_THAT(subdomains, Eq(std::vector{reshuffle::Block{0, 5}, reshuffle::Block{5, 11}}));
 }
 
 #endif//RESHUFFLE_DATA_DISTRIBUTION_TEST_HPP
