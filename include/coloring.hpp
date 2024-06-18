87b269f1e5995d94bb0e4f7ee4a970b6f2e46bd7
Santiago Narvaez <santiagonar1@gmail.com>
2024-06-18 10:07:03 +0200

    refactor: move BlockWise to its own header
    We are planning to create additional data distributions,
    so having them on a separate header will help keeping
    things organized.

--- include/coloring.hpp
+++ include/coloring.hpp
@@ -1,6 +1,7 @@
 #ifndef RESHUFFLE_COLORING_HPP
 #define RESHUFFLE_COLORING_HPP
 
+#include "data_distribution.hpp"
 #include "dimensions.hpp"
 #include "indices.hpp"
 #include "left_closed_range.hpp"
@@ -12,32 +13,6 @@
 #include <numeric>
 
 namespace reshuffle {
-    class BlockWise {
-    private:
-        const int _num_blocks;
-
-        [[nodiscard]] int get_min_values_per_block(int num_values) const {
-            return num_values / _num_blocks;
-        }
-
-    public:
-        explicit BlockWise(int num_blocks) : _num_blocks(num_blocks) {}
-
-        [[nodiscard]] auto get_subdomains(int num_values) const {
-            const auto min_values_per_block = get_min_values_per_block(num_values);
-            std::vector<internal::Subdomain> subdomains{};
-
-            for (int i = 0; i < min_values_per_block * _num_blocks; i += min_values_per_block) {
-                const auto starting_index = i;
-                const auto last_index = starting_index + min_values_per_block;
-                subdomains.emplace_back(starting_index, last_index);
-            }
-
-            subdomains.back().second = num_values;
-            return subdomains;
-        }
-    };
-
     namespace internal {
         rank_id get_color(const std::vector<Subdomain> &subdomains, int i) {
             auto it = std::ranges::find_if(
---
+++ include/data_distribution.hpp
@@ -0,0 +1,36 @@
+#ifndef RESHUFFLE_DATA_DISTRIBUTION_HPP
+#define RESHUFFLE_DATA_DISTRIBUTION_HPP
+
+#include "subdomain.hpp"
+#include <vector>
+
+namespace reshuffle {
+    class BlockWise {
+    private:
+        const int _num_blocks;
+
+        [[nodiscard]] int get_min_values_per_block(int num_values) const {
+            return num_values / _num_blocks;
+        }
+
+    public:
+        explicit BlockWise(int num_blocks) : _num_blocks(num_blocks) {}
+
+        [[nodiscard]] auto get_subdomains(int num_values) const {
+            const auto min_values_per_block = get_min_values_per_block(num_values);
+            std::vector<internal::Subdomain> subdomains{};
+
+            for (int i = 0; i < min_values_per_block * _num_blocks; i += min_values_per_block) {
+                const auto starting_index = i;
+                const auto last_index = starting_index + min_values_per_block;
+                subdomains.emplace_back(starting_index, last_index);
+            }
+
+            subdomains.back().second = num_values;
+            return subdomains;
+        }
+    };
+}// namespace reshuffle
+
+
+#endif//RESHUFFLE_DATA_DISTRIBUTION_HPP
