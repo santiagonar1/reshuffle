4cc42403ef800e6e54a1bce4de8d7435f83c53b1
Santiago Narvaez <santiagonar1@gmail.com>
2024-09-17 15:52:37 +0200

    feat(demo): implement operator<< for matrix
    Instead of the print function, we can simply implement the
    operator<<, which reduces the lines of code.

--- demos/demo.cpp
+++ demos/demo.cpp
@@ -11,9 +11,9 @@ using DataDistribution2D = std::array<reshuffle::BlockCyclic, 2>;
 
 bool is_root();
 
-void print(const Matrix &matrix);
-
 Matrix init_matrix(int num_rows, int num_columns);
+
+std::ostream &operator<<(std::ostream &os, const Matrix &matrix);
 
 int main() {
     constexpr int num_rows = 20;
@@ -42,13 +42,11 @@ int main() {
             {reshuffle::make_block_wise(num_columns, 2), reshuffle::make_block_wise(num_rows, 2)},
             {reshuffle::make_block_wise(num_columns, 1), reshuffle::make_block_wise(num_rows, 4)}};
 
+    if (is_root()) { std::cout << matrix << std::endl; }
 
     for (int i = 1; i < distributions.size(); ++i) {
         matrix = reshuffle::shuffle(matrix, MPI_COMM_WORLD, distributions[i - 1], distributions[i]);
-        if (is_root()) {
-            print(matrix);
-            std::cout << "\n";
-        }
+        if (is_root()) { std::cout << matrix << std::endl; }
     }
 
     MPI_Finalize();
@@ -59,13 +57,6 @@ bool is_root() {
     int rank{};
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     return rank == 0;
-}
-
-void print(const Matrix &matrix) {
-    for (const auto &row: matrix) {
-        for (const auto &value: row) { std::cout << value << " "; }
-        std::cout << std::endl;
-    }
 }
 
 Matrix init_matrix(const int num_rows, const int num_columns) {
@@ -80,4 +71,13 @@ Matrix init_matrix(const int num_rows, const int num_columns) {
     }
 
     return matrix;
+}
+
+std::ostream &operator<<(std::ostream &os, const Matrix &matrix) {
+    for (const auto &row: matrix) {
+        for (const auto &value: row) { os << value << " "; }
+        os << std::endl;
+    }
+
+    return os;
 }
