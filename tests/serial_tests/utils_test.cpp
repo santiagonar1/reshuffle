8f9ea651addb1fd6cc46013a38d78960c229121f
Santiago Narvaez <santiagonar1@gmail.com>
2024-09-25 10:51:33 +0200

    test(utils): add test for to_matrix function

--- tests/serial_tests/utils_test.cpp
+++ tests/serial_tests/utils_test.cpp
@@ -13,3 +13,10 @@ TEST(Combine, CalculatesTheCartesianProductOfTwoVectors) {
     EXPECT_THAT(combine(v1, v2), Eq(std::vector{std::pair{1, 3}, std::pair{2, 3}, std::pair{1, 4},
                                                 std::pair{2, 4}}));
 }
+
+TEST(ToMatrix, ConstructsAMatrixFromA1DArray) {
+    constexpr auto dimensions = Dimension<2>{2, 2};
+    const auto v = std::vector{1, 2, 3, 4};
+
+    EXPECT_THAT(to_matrix(v, dimensions), Eq(std::vector{std::vector{1, 2}, std::vector{3, 4}}));
+}
