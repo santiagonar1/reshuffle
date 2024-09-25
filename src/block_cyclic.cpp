a5c5542b254feeb4e9aaf8d582c3ac6279182a98
Santiago Narvaez <santiagonar1@gmail.com>
2024-09-25 09:32:23 +0200

    refactor(block-cyclic): change type attribute get_num_values to rank_id

--- src/block_cyclic.cpp
+++ src/block_cyclic.cpp
@@ -56,7 +56,7 @@ namespace reshuffle {
 
     auto BlockCyclic::get_num_values() const -> int { return _num_values; }
 
-    auto BlockCyclic::get_num_values(const int rank_id) const -> int {
+    auto BlockCyclic::get_num_values(const rank_id rank_id) const -> int {
         if (rank_id >= _num_ranks) { return 0; }
 
         if (rank_id < 0) { throw std::invalid_argument("rank_id cannot be negative"); }
