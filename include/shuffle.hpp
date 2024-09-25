bf0c0d68646482860229599dcce697148ce6bc7e
Santiago Narvaez <santiagonar1@gmail.com>
2024-09-25 11:01:33 +0200

    refactor: move utility functions from shuffle to utils

--- include/shuffle.hpp
+++ include/shuffle.hpp
@@ -93,17 +93,6 @@ namespace reshuffle {
 
             return scatter_values_from_root(std::span{all_values}, destiny_comm, all_coloring);
         }
-
-        inline auto have_same_num_values(const BlockCyclic &first, const BlockCyclic &second)
-                -> bool {
-            return first.get_num_values() == second.get_num_values();
-        }
-
-        inline auto have_same_num_values(const std::array<BlockCyclic, 2> &first,
-                                         const std::array<BlockCyclic, 2> &second) -> bool {
-            return have_same_num_values(first[0], second[0]) and
-                   have_same_num_values(first[1], second[1]);
-        }
     }// namespace internal
 
     template<concepts::ContiguousContainer C>
--- include/utils.hpp
+++ include/utils.hpp
@@ -1,6 +1,7 @@
 #ifndef RESHUFFLE_UTILS_HPP
 #define RESHUFFLE_UTILS_HPP
 
+#include "block_cyclic.hpp"
 #include "dimensions.hpp"
 
 namespace reshuffle::internal {
@@ -30,6 +31,16 @@ namespace reshuffle::internal {
         }
 
         return matrix;
+    }
+
+    inline auto have_same_num_values(const BlockCyclic &first, const BlockCyclic &second) -> bool {
+        return first.get_num_values() == second.get_num_values();
+    }
+
+    inline auto have_same_num_values(const std::array<BlockCyclic, 2> &first,
+                                     const std::array<BlockCyclic, 2> &second) -> bool {
+        return have_same_num_values(first[0], second[0]) and
+               have_same_num_values(first[1], second[1]);
     }
 }// namespace reshuffle::internal
 
