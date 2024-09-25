53299b070978f378f23c4b7eb6e5891240ea25f5
Santiago Narvaez <santiagonar1@gmail.com>
2024-09-25 10:46:54 +0200

    test(utils): add test for combine function

--- tests/serial_tests/utils_test.cpp
+++ tests/serial_tests/utils_test.cpp
@@ -4,3 +4,12 @@
 #include <utils.hpp>
 
 using testing::Eq;
+using namespace reshuffle::internal;
+
+TEST(Combine, CalculatesTheCartesianProductOfTwoVectors) {
+    const auto v1 = std::vector{1, 2};
+    const auto v2 = std::vector{3, 4};
+
+    EXPECT_THAT(combine(v1, v2), Eq(std::vector{std::pair{1, 3}, std::pair{2, 3}, std::pair{1, 4},
+                                                std::pair{2, 4}}));
+}
