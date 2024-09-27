73a59561cc08da75db7217712641bc620abf7d54
Santiago Narvaez <santiagonar1@gmail.com>
2024-09-27 15:48:32 +0200

    refactor(mpi-utils): make get_num_ranks throw if comm is null

--- src/mpi_utils.cpp
+++ src/mpi_utils.cpp
@@ -20,6 +20,11 @@ namespace reshuffle::internal {
 
     auto get_num_ranks(const MPI_Comm &comm) -> int {
         int num_ranks{};
+
+        if (comm == MPI_COMM_NULL) {
+            throw std::invalid_argument("Invalid MPI_COMM_NULL communicator");
+        }
+
         MPI_Comm_size(comm, &num_ranks);
         return num_ranks;
     }
