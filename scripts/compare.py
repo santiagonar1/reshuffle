2ad1a2184f211c0955edd9bff52cf0cd1f73c421
Santiago Narvaez <santiago.narvaez@tum.de>
2025-02-21 13:32:30 +0100

    refactor(benchmark): move compare functionality to own script

--- scripts/compare.py
+++ scripts/compare.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import argparse
 
-
 from common import utils
 
 
@@ -28,10 +27,17 @@ def get_args():
     )
 
     parser.add_argument(
-        "--result_file",
+        "--base_file",
         type=str,
-        default="benchmark_example/results.json",
+        required=True,
         help="Path to the JSON file with benchmark results"
+    )
+
+    parser.add_argument(
+        "--compare_with",
+        type=str,
+        required=True,
+        help="Compare the results of the current benchmark with the results of the given benchmark"
     )
 
     parser.add_argument(
@@ -41,6 +47,8 @@ def get_args():
         help="Show the results of the benchmarks that contain the given string in their name"
     )
 
+
+
     return parser.parse_args()
 
 
@@ -48,14 +56,31 @@ def main():
     args = get_args()
 
     metric_names = args.metrics
-    results_fname = args.result_file
+    base_file = args.base_file
     benchmark_filter = args.benchmark_filter
+    compare_with = args.compare_with
 
-    results = utils.load_benchmark_results(results_fname, benchmark_filter)
+    results = utils.load_benchmark_results(base_file, benchmark_filter)
     metrics = utils.get_metrics(results, metric_names)
 
     for metric_name in metric_names:
         plot_metrics(metrics, metric_name)
+
+    if compare_with:
+        compare_results = utils.load_benchmark_results(compare_with, benchmark_filter)
+        compare_metrics = utils.get_metrics(compare_results, metric_names)
+
+        for metric_name in metric_names:
+            for experiment, metric_data in metrics.items():
+                plt.plot(metric_data["num_elements"], metric_data[metric_name], label=f"{base_file}")
+                plt.plot(compare_metrics[experiment]["num_elements"], compare_metrics[experiment][metric_name],
+                         label=f"{compare_with}")
+
+                plt.title(f"{experiment}")
+                plt.xlabel("Number of Elements")
+                plt.ylabel("Value")
+                plt.legend()
+                plt.show()
 
 
 if __name__ == "__main__":
