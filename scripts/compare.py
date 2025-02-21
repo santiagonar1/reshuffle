7dd9da43da0024585a907e033537b0470a644e83
Santiago Narvaez <santiago.narvaez@tum.de>
2025-02-21 13:35:02 +0100

    refactor(benchmark): improve compare script

--- scripts/compare.py
+++ scripts/compare.py
@@ -4,19 +4,38 @@ import argparse
 from common import utils
 
 
-def plot_metrics(metrics, metric_name: str):
-    for experiment, metric_data in metrics.items():
-        plt.plot(metric_data["num_elements"], metric_data[metric_name], label=f"{experiment}")
+def plot_comparison(base_metrics, compare_metrics, metric_name: str, experiment: str):
+    plt.plot(base_metrics[experiment]["num_elements"], base_metrics[experiment][metric_name], label=f"Base")
+    plt.plot(compare_metrics[experiment]["num_elements"], compare_metrics[experiment][metric_name],
+             label=f"Contender")
 
-    plt.title(f"{metric_name}")
+    plt.title(f"{experiment}")
     plt.xlabel("Number of Elements")
     plt.ylabel("Value")
     plt.legend()
     plt.show()
 
 
+def get_common_experiments(base_metrics, contender_metrics):
+    return set(base_metrics.keys()).intersection(contender_metrics.keys())
+
+
 def get_args():
     parser = argparse.ArgumentParser(description="Plot benchmark metrics.")
+
+    parser.add_argument(
+        "--base_file",
+        type=str,
+        required=True,
+        help="Path to the JSON file with benchmark results"
+    )
+
+    parser.add_argument(
+        "--contender_file",
+        type=str,
+        required=True,
+        help="Compare the results of the base benchmark with the results of this benchmark"
+    )
 
     parser.add_argument(
         "--metrics",
@@ -27,27 +46,11 @@ def get_args():
     )
 
     parser.add_argument(
-        "--base_file",
-        type=str,
-        required=True,
-        help="Path to the JSON file with benchmark results"
-    )
-
-    parser.add_argument(
-        "--compare_with",
-        type=str,
-        required=True,
-        help="Compare the results of the current benchmark with the results of the given benchmark"
-    )
-
-    parser.add_argument(
         "--benchmark_filter",
         required=False,
         type=str,
         help="Show the results of the benchmarks that contain the given string in their name"
     )
-
-
 
     return parser.parse_args()
 
@@ -58,29 +61,19 @@ def main():
     metric_names = args.metrics
     base_file = args.base_file
     benchmark_filter = args.benchmark_filter
-    compare_with = args.compare_with
+    contender_file = args.contender_file
 
-    results = utils.load_benchmark_results(base_file, benchmark_filter)
-    metrics = utils.get_metrics(results, metric_names)
+    base_results = utils.load_benchmark_results(base_file, benchmark_filter)
+    base_metrics = utils.get_metrics(base_results, metric_names)
+
+    contender_results = utils.load_benchmark_results(contender_file, benchmark_filter)
+    contender_metrics = utils.get_metrics(contender_results, metric_names)
+
+    common_experiments = get_common_experiments(base_metrics, contender_metrics)
 
     for metric_name in metric_names:
-        plot_metrics(metrics, metric_name)
-
-    if compare_with:
-        compare_results = utils.load_benchmark_results(compare_with, benchmark_filter)
-        compare_metrics = utils.get_metrics(compare_results, metric_names)
-
-        for metric_name in metric_names:
-            for experiment, metric_data in metrics.items():
-                plt.plot(metric_data["num_elements"], metric_data[metric_name], label=f"{base_file}")
-                plt.plot(compare_metrics[experiment]["num_elements"], compare_metrics[experiment][metric_name],
-                         label=f"{compare_with}")
-
-                plt.title(f"{experiment}")
-                plt.xlabel("Number of Elements")
-                plt.ylabel("Value")
-                plt.legend()
-                plt.show()
+        for experiment in common_experiments:
+            plot_comparison(base_metrics, contender_metrics, metric_name, experiment)
 
 
 if __name__ == "__main__":
