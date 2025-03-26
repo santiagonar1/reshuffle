936cb2700c3650c1736f389f09083d1c30d2223c
Santiago Narvaez <santiago.narvaez@tum.de>
2025-03-26 15:23:55 +0100

    fix(build): remove unnecessary LAPACK/BLAS packages
    When I was just testing how to link the code with ScaLAPACK, I trusted
    the example given by the A.I., where both LAPACK and BLAS were being
    linked. Well, it turns out they are not required at all. Thus, I just
    went ahead and remove them from the build requirements.

--- cmake/FindScalapackPackages.cmake
+++ cmake/FindScalapackPackages.cmake
@@ -1,7 +1,3 @@
-# Find the required basic packages
-find_package(LAPACK REQUIRED)
-find_package(BLAS REQUIRED)
-
 # Try pkg-config first for ScaLAPACK
 find_package(PkgConfig QUIET)
 if (PKG_CONFIG_FOUND)
@@ -28,10 +24,7 @@ endif ()
 add_library(ScalapackLibrary INTERFACE)
 target_link_libraries(ScalapackLibrary
         INTERFACE
-        ${SCALAPACK_LIBRARIES}
-        ${LAPACK_LIBRARIES}
-        ${BLAS_LIBRARIES}
-)
+        ${SCALAPACK_LIBRARIES})
 
 if (SCALAPACK_INCLUDE_DIRS)
     target_include_directories(ScalapackLibrary
