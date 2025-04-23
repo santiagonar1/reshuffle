6f7f265ae86edbbcc793106462a29e4ebfba98dd
Santiago Narvaez <santiago.narvaez@tum.de>
2025-04-23 16:22:19 +0200

    feat: add methods to map from N Dimensions to 1D and another way around

--- include/coordinates.hpp
+++ include/coordinates.hpp
@@ -1,11 +1,63 @@
 #ifndef RESHUFFLE_COORDINATES_HPP
 #define RESHUFFLE_COORDINATES_HPP
 
+#include "dimensions.hpp"
+
 namespace reshuffle::internal {
     struct Coordinates2D {
         int x_coordinate{};
         int y_coordinate{};
     };
+
+    template<std::size_t N>
+    using Coordinates = std::array<int, N>;
+
+    template<std::size_t N>
+    [[nodiscard]] auto map_indices(const Coordinates<N> &coordinates,
+                                   const Dimensions<N> &dimensions) -> int {
+        const auto start = static_cast<int>(coordinates.size()) - 1;
+
+        int offset = 0;
+        int stride = 1;
+
+        // Start from the rightmost dimension (least significant)
+        for (int i = start; i >= 0; --i) {
+            if (coordinates[i] == dimensions[i]) {
+                throw std::invalid_argument("Coordinate should be smaller than the dimension");
+            }
+            offset += stride * coordinates[i];
+            stride *= dimensions[i];
+        }
+
+        return offset;
+    }
+
+    template<std::size_t N>
+    [[nodiscard]] auto map_index(int index, const Dimensions<N> &dimensions) -> Coordinates<N> {
+
+        if (index < 0) { throw std::invalid_argument("Index cannot be negative"); }
+
+        const auto max_index = calc_total_num_values(dimensions) - 1;
+        if (index > max_index) {
+            throw std::invalid_argument("Index exceeds the total size of the dimensions");
+        }
+
+
+        Coordinates<N> coordinates{};
+        // Start from the rightmost dimension (least significant)
+        for (int i = N - 1; i >= 0; --i) {
+            coordinates[i] = index % dimensions[i];
+            index /= dimensions[i];
+        }
+
+        // Check if the index was too large for the given dimensions
+        if (index > 0) {
+            throw std::invalid_argument("Index exceeds the total size of the dimensions");
+        }
+
+        return coordinates;
+    }
+
 }// namespace reshuffle::internal
 
 #endif//RESHUFFLE_COORDINATES_HPP
---
+++ tests/serial_tests/coordinates_test.cpp
@@ -0,0 +1,60 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <coordinates.hpp>
+
+using namespace reshuffle::internal;
+
+using testing::Eq;
+
+TEST(Coordinates, CanBeCreatedFromArray) {
+    const auto coordinates = Coordinates{1, 2};
+    EXPECT_THAT(coordinates[0], Eq(1));
+    EXPECT_THAT(coordinates[1], Eq(2));
+}
+
+TEST(MapIndices, MapsAMultiDimensionCoordinateTo1D) {
+    const auto coordinates = Coordinates{0, 1};
+    const auto dimensions = Dimensions{2, 4};
+
+    EXPECT_THAT(map_indices(coordinates, dimensions), Eq(1));
+}
+
+TEST(MapIndices, UsesRowMajorFormat) {
+    const auto dimensions = Dimensions{2, 4};
+
+    EXPECT_THAT(map_indices(Coordinates{0, 3}, dimensions), Eq(3));
+    EXPECT_THAT(map_indices(Coordinates{1, 0}, dimensions), Eq(4));
+}
+
+TEST(MapIndices, EachCoordianteIndexIsBoundedByTheRespectiveDimension) {
+    const auto dimensions = Dimensions{2, 4};
+    const auto coordinates_too_many_rows = Coordinates{2, 0};
+    const auto coordinates_too_many_columns = Coordinates{0, 4};
+
+    EXPECT_THROW(auto _ = map_indices(coordinates_too_many_rows, dimensions),
+                 std::invalid_argument);
+    EXPECT_THROW(auto _ = map_indices(coordinates_too_many_columns, dimensions),
+                 std::invalid_argument);
+}
+
+TEST(MapIndex, MapsA1DIndexIntoAMultiDimensionalCoordinate) {
+    const auto dimensions = Dimensions{2, 4};
+
+    EXPECT_THAT(map_index(0, dimensions), Eq(Coordinates{0, 0}));
+}
+
+TEST(MapIndex, UsesRowMajorFormat) {
+    const auto dimensions = Dimensions{2, 4};
+
+    EXPECT_THAT(map_index(0, dimensions), Eq(Coordinates{0, 0}));
+    EXPECT_THAT(map_index(1, dimensions), Eq(Coordinates{0, 1}));
+    EXPECT_THAT(map_index(dimensions[1], dimensions), Eq(Coordinates{1, 0}));
+}
+
+TEST(MapIndex, ThrowsIf1DIndexOutsideOfNumValuesHeldByDimensions) {
+    const auto dimensions = Dimensions{2, 4};
+    const auto num_values = calc_total_num_values(dimensions);
+
+    EXPECT_THROW(auto _ = map_index(num_values, dimensions), std::invalid_argument);
+}
