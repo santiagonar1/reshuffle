dc655fcc1c7533b1274b7062759ceff7512dea00
Santiago Narvaez <santiago.narvaez@tum.de>
2025-04-29 11:50:03 +0200

    feat: replace cartesian view with functions
    I tried several times to use the cartesian grid view, but it was always
    problematic. The pipeline was not supported (this also applies to
    "proper" implementations, such as the one in v3-ranges), it would
    produce tuples that needed to be unpacked, and, most importantly, the
    code was hard to understand. Thus, I opted to implement the cartesian
    product for up to 3 dimensions and assume the cost of copying those
    values.

---
+++ include/cartesian_product.hpp
@@ -0,0 +1,52 @@
+#ifndef CARTESIAN_PRODUCT_HPP
+#define CARTESIAN_PRODUCT_HPP
+
+#include <iterator>
+#include <ranges>
+#include <tuple>
+#include <utility>
+
+namespace reshuffle::dev::internal {
+
+    template<typename T>
+    [[nodiscard]] auto get_cartesian_product(const std::array<std::vector<T>, 1> &blocks)
+            -> std::vector<std::array<T, 1>> {
+        const auto vector = blocks | std::views::join;
+        auto result = std::vector<std::array<T, 1>>{};
+
+        for (const auto &block: vector) { result.emplace_back(std::array{block}); }
+
+        return result;
+    }
+
+    template<typename T>
+    [[nodiscard]] auto get_cartesian_product(const std::array<std::vector<T>, 2> &blocks)
+            -> std::vector<std::array<T, 2>> {
+        auto result = std::vector<std::array<T, 2>>{};
+
+        for (const auto &v1: blocks[0]) {
+            for (const auto &v2: blocks[1]) { result.emplace_back(std::array{v1, v2}); }
+        }
+
+        return result;
+    }
+
+    template<typename T>
+    [[nodiscard]] auto get_cartesian_product(const std::array<std::vector<T>, 3> &blocks)
+            -> std::vector<std::array<T, 3>> {
+        auto result = std::vector<std::array<T, 3>>{};
+
+        for (const auto &v1: blocks[0]) {
+            for (const auto &v2: blocks[1]) {
+                for (const auto &v3: blocks[2]) { result.emplace_back(std::array{v1, v2, v3}); }
+            }
+        }
+
+        return result;
+    }
+
+}// namespace reshuffle
+
+
+
+#endif//CARTESIAN_PRODUCT_HPP
---
+++ tests/serial_tests/cartesian_product_test.cpp
@@ -0,0 +1,45 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <cartesian_product.hpp>
+
+using namespace reshuffle::dev::internal;
+
+using testing::Eq;
+
+
+TEST(GetCartesianProduct, CalculatesTheCartesianProductOfVectorsInArray) {
+    const auto v1 = std::vector{1, 2};
+    const auto array = std::array{v1};
+
+    const auto expected = std::vector<std::array<int, 1>>{{1}, {2}};
+    EXPECT_THAT(get_cartesian_product(array), Eq(expected));
+}
+
+TEST(GetCartesianProduct, WorksIn2D) {
+    const auto v1 = std::vector{1, 2};
+    const auto v2 = std::vector{3, 4};
+    const auto array = std::array{v1, v2};
+
+    const auto expected = std::vector<std::array<int, 2>>{{1, 3}, {1, 4}, {2, 3}, {2, 4}};
+    EXPECT_THAT(get_cartesian_product(array), Eq(expected));
+}
+
+TEST(GetCartesianProduct, WorksIn3D) {
+    const auto v1 = std::vector{1, 2};
+    const auto v2 = std::vector{3, 4};
+    const auto v3 = std::vector{5, 6};
+    const auto array = std::array{v1, v2, v3};
+
+    const auto expected = std::vector<std::array<int, 3>>{
+            {1, 3, 5}, {1, 3, 6}, {1, 4, 5}, {1, 4, 6}, {2, 3, 5}, {2, 3, 6}, {2, 4, 5}, {2, 4, 6}};
+    EXPECT_THAT(get_cartesian_product(array), Eq(expected));
+}
+
+TEST(CartesianProduct, WithEmtpyVectorIsEmtpy) {
+    const auto v1 = std::vector{1, 2};
+    const auto empty = std::vector<int>{};
+    const auto array = std::array{v1, empty};
+
+    EXPECT_TRUE(get_cartesian_product(array).empty());
+}
