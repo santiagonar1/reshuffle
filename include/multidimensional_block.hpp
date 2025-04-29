321275d15a8017d58ba285e6817a9e745d82d714
Santiago Narvaez <santiago.narvaez@tum.de>
2025-04-29 17:18:26 +0200

    feat: add function to get owner coordinates MultidimensionalBlock

--- include/multidimensional_block.hpp
+++ include/multidimensional_block.hpp
@@ -6,6 +6,17 @@
 namespace reshuffle::dev {
     template<std::size_t N>
     using MultidimensionalBlock = std::array<Block, N>;
-}
+
+    template<std::size_t N>
+    auto get_owner_coordinates(const MultidimensionalBlock<N> &multidimensional_block)
+            -> reshuffle::internal::Coordinates<N> {
+        auto owner_coordinates = reshuffle::internal::Coordinates<N>{};
+        auto reversed_multidimensional_block = multidimensional_block | std::views::reverse;
+        for (int i = 0; i < N; ++i) {
+            owner_coordinates[i] = reversed_multidimensional_block[i].get_owner();
+        }
+        return owner_coordinates;
+    }
+}// namespace reshuffle::dev
 
 #endif//MULTIDIMENSIONAL_BLOCK_HPP
---
+++ tests/serial_tests/multidimensional_block_test.cpp
@@ -0,0 +1,20 @@
+#include "coordinates.hpp"
+
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <multidimensional_block.hpp>
+
+using namespace reshuffle::dev;
+
+using testing::Eq;
+
+TEST(GetOwnerCoordinates, ReturnsTheCoordinatesOfProcessorThatOwnsMultiblock) {
+    const auto x_block = Block{{0, 2}, 0};
+    const auto y_block = Block{{1, 3}, 1};
+    const auto multiblock = MultidimensionalBlock{x_block, y_block};
+
+    const auto expected = reshuffle::internal::Coordinates{1, 0};
+    EXPECT_THAT(get_owner_coordinates(multiblock), Eq(expected));
+}
