123f668cb704fd02ce95100270ad8e072fe42a6c
Santiago Narvaez <santiago.narvaez@tum.de>
2025-05-01 17:28:49 +0200

    fix: cartesian product should change first x, then y, then z
    This is easier to explain in 2D. Let's assume we have an array of
    vectors, where the first vector corresponds to the Blocks in X and
    the second to the Blocks in Y. When we combine them using the
    cartesian product, we want to generate a vector of
    MultidimensionalBlocks (MDs) (i.e., vector of arrays), such that they
    are ordered in the same way as the processor grid: using a row major
    scheme. This in turn means that we need to move first on the X-axis
    (i.e., the horizontal one), and after in the Y-axis (i.e., vertical).
    Thus, if we produce MDs of the form (b1, b2), b1 should be iterated
    first.

--- include/cartesian_product.hpp
+++ include/cartesian_product.hpp
@@ -24,8 +24,11 @@ namespace reshuffle::dev::internal {
             -> std::vector<std::array<T, 2>> {
         auto result = std::vector<std::array<T, 2>>{};
 
-        for (const auto &v1: blocks[0]) {
-            for (const auto &v2: blocks[1]) { result.emplace_back(std::array{v1, v2}); }
+        // We assume a row-major ordering through the code, which implies that we first move in the
+        // x-axis (i.e., horizontal) than in the y-axis (vertical). Since we assume that the first
+        // block corresponds to x and the second to y, we need to modify first v1 and then v2.
+        for (const auto &v2: blocks[1]) {
+            for (const auto &v1: blocks[0]) { result.emplace_back(std::array{v1, v2}); }
         }
 
         return result;
@@ -36,17 +39,16 @@ namespace reshuffle::dev::internal {
             -> std::vector<std::array<T, 3>> {
         auto result = std::vector<std::array<T, 3>>{};
 
-        for (const auto &v1: blocks[0]) {
+        for (const auto &v3: blocks[2]) {
             for (const auto &v2: blocks[1]) {
-                for (const auto &v3: blocks[2]) { result.emplace_back(std::array{v1, v2, v3}); }
+                for (const auto &v1: blocks[0]) { result.emplace_back(std::array{v1, v2, v3}); }
             }
         }
 
         return result;
     }
 
-}// namespace reshuffle
-
+}// namespace reshuffle::dev::internal
 
 
 #endif//CARTESIAN_PRODUCT_HPP
