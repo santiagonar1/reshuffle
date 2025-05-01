9d7fd64617f0a0f29f7301cfe8fdaae2c22ac7fc
Santiago Narvaez <santiago.narvaez@tum.de>
2025-05-01 16:58:46 +0200

    refactor: add a note on why we need to reverse the block

--- include/multidimensional_block.hpp
+++ include/multidimensional_block.hpp
@@ -11,6 +11,13 @@ namespace reshuffle::dev {
     auto get_owner_coordinates(const MultidimensionalBlock<N> &multidimensional_block)
             -> reshuffle::internal::Coordinates<N> {
         auto owner_coordinates = reshuffle::internal::Coordinates<N>{};
+        // Note: the easies way to understand why we need to reverse the coordinates is with an
+        // example. So far through the code we assume that MultidimensionalBlock stores the Blocks
+        // like {X, Y} (i.e., first the horizontal, then the vertical). Nonetheless,
+        // the processor grid assumes a row major ordering. Let's assume that the coordinates
+        // stored in {X.get_owner(), Y.get_owner()} are {1, 0}, which, assuming a 2x2 division,
+        // would locate the block in the top right corner. Now, this block should belong to
+        // the processor with coordinates {0, 1}, with rank_id 1.
         auto reversed_multidimensional_block = multidimensional_block | std::views::reverse;
         for (int i = 0; i < N; ++i) {
             owner_coordinates[i] = reversed_multidimensional_block[i].get_owner();
