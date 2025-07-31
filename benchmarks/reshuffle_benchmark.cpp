1264a55c4b44fe18cedeb39d3197a9bd5c85aa45
Santiago Narvaez <santiago.narvaez@tum.de>
2025-07-31 15:35:19 +0200

    feat(benchmark): add benchmark for serialization
    So far we were only testing the time that it took to run the code for
    fundamental datatypes. Now we include a benchmark to test how much
    overhead is created by serialization.

--- benchmarks/reshuffle_benchmark.cpp
+++ benchmarks/reshuffle_benchmark.cpp
@@ -4,13 +4,16 @@
 
 #include <reshuffle.hpp>
 
+#include "autopas_particle.hpp"
 #include "null_reporter.hpp"
 
 using namespace reshuffle;
 
 using SendType = double;
+using SerializationType = MoleculeLJ;
 
-double time_shuffle(const std::vector<SendType> &local_values, const Context<1> &initial_context,
+template<typename T>
+double time_shuffle(const std::vector<T> &local_values, const Context<1> &initial_context,
                     const Context<1> &final_context) {
     // Do the work and time it on each proc
     const auto start = std::chrono::high_resolution_clock::now();
@@ -57,6 +60,33 @@ void shuffle_from_N_to_one(benchmark::State &state) {
     }
 }
 
+void shuffle_serialization_from_N_to_one(benchmark::State &state) {
+    const auto num_global_values = static_cast<int>(state.range(0));
+
+    const auto num_ranks = reshuffle::mpi::get_num_ranks(MPI_COMM_WORLD);
+
+    if (num_global_values % num_ranks != 0) {
+        throw std::runtime_error("Number of values not divisible by number of ranks");
+    }
+
+    const auto values_per_rank = num_global_values / num_ranks;
+    const auto original_values = std::vector<SerializationType>(values_per_rank);
+
+    const auto initial_processor_grid = ProcessorGrid<1>{{num_ranks}};
+    const auto initial_distribution =
+            make_block_wise_distribution({num_global_values}, initial_processor_grid);
+    const auto initial_context = Context{initial_distribution, MPI_COMM_WORLD};
+
+    const auto final_processor_grid = ProcessorGrid<1>{{1}};
+    const auto final_distribution =
+            make_block_wise_distribution({num_global_values}, final_processor_grid);
+    const auto final_context = Context{final_distribution, MPI_COMM_WORLD};
+
+    while (state.KeepRunning()) {
+        state.SetIterationTime(time_shuffle(original_values, initial_context, final_context));
+    }
+}
+
 void shuffle_from_one_to_N(benchmark::State &state) {
     const auto num_global_values = static_cast<int>(state.range(0));
 
@@ -69,6 +99,34 @@ void shuffle_from_one_to_N(benchmark::State &state) {
     const auto original_values = reshuffle::mpi::is_root(MPI_COMM_WORLD)
                                          ? std::vector<SendType>(num_global_values)
                                          : std::vector<SendType>{};
+
+    const auto initial_processor_grid = ProcessorGrid<1>{{1}};
+    const auto initial_distribution =
+            make_block_wise_distribution({num_global_values}, initial_processor_grid);
+    const auto initial_context = Context{initial_distribution, MPI_COMM_WORLD};
+
+    const auto final_processor_grid = ProcessorGrid<1>{{num_ranks}};
+    const auto final_distribution =
+            make_block_wise_distribution({num_global_values}, final_processor_grid);
+    const auto final_context = Context{final_distribution, MPI_COMM_WORLD};
+
+    while (state.KeepRunning()) {
+        state.SetIterationTime(time_shuffle(original_values, initial_context, final_context));
+    }
+}
+
+void shuffle_serialization_from_one_to_N(benchmark::State &state) {
+    const auto num_global_values = static_cast<int>(state.range(0));
+
+    const auto num_ranks = reshuffle::mpi::get_num_ranks(MPI_COMM_WORLD);
+
+    if (num_global_values % num_ranks != 0) {
+        throw std::runtime_error("Number of values not divisible by number of ranks");
+    }
+
+    const auto original_values = reshuffle::mpi::is_root(MPI_COMM_WORLD)
+                                         ? std::vector<SerializationType>(num_global_values)
+                                         : std::vector<SerializationType>{};
 
     const auto initial_processor_grid = ProcessorGrid<1>{{1}};
     const auto initial_distribution =
@@ -133,6 +191,32 @@ void shuffle_from_N_to_N(benchmark::State &state) {
     }
 }
 
+void shuffle_serialization_from_N_to_N(benchmark::State &state) {
+    const auto num_global_values = static_cast<int>(state.range(0));
+
+    const auto num_ranks = reshuffle::mpi::get_num_ranks(MPI_COMM_WORLD);
+
+    if (num_global_values % num_ranks != 0) {
+        throw std::runtime_error("Number of values not divisible by number of ranks");
+    }
+
+    const auto values_per_rank = num_global_values / num_ranks;
+    const auto original_values = std::vector<SerializationType>(values_per_rank);
+
+    const auto initial_processor_grid = ProcessorGrid<1>{{num_ranks}};
+    const auto initial_distribution =
+            make_block_wise_distribution({num_global_values}, initial_processor_grid);
+    const auto initial_context = Context{initial_distribution, MPI_COMM_WORLD};
+
+    const auto final_processor_grid = ProcessorGrid<1>{{num_ranks}};
+    const auto final_distribution = BlockCyclic{{num_global_values}, {10}, final_processor_grid};
+    const auto final_context = Context{final_distribution, MPI_COMM_WORLD};
+
+    while (state.KeepRunning()) {
+        state.SetIterationTime(time_shuffle(original_values, initial_context, final_context));
+    }
+}
+
 void shuffle_reduction(benchmark::State &state) {
     const auto num_global_values = static_cast<int>(state.range(0));
 
@@ -169,10 +253,13 @@ constexpr auto LIMIT = 100'000;
 constexpr auto STEP = 10'000;
 
 BENCHMARK(shuffle_from_N_to_one)->UseManualTime()->DenseRange(START, LIMIT, STEP);
+BENCHMARK(shuffle_serialization_from_N_to_one)->UseManualTime()->DenseRange(START, LIMIT, STEP);
 BENCHMARK(shuffle_from_one_to_N)->UseManualTime()->DenseRange(START, LIMIT, STEP);
+BENCHMARK(shuffle_serialization_from_one_to_N)->UseManualTime()->DenseRange(START, LIMIT, STEP);
 BENCHMARK(shuffle_reduction)->UseManualTime()->DenseRange(START, LIMIT, STEP);
 BENCHMARK(shuffle_from_N_to_N_same_distribution)->UseManualTime()->DenseRange(START, LIMIT, STEP);
 BENCHMARK(shuffle_from_N_to_N)->UseManualTime()->DenseRange(START, LIMIT, STEP);
+BENCHMARK(shuffle_serialization_from_N_to_N)->UseManualTime()->DenseRange(START, LIMIT, STEP);
 
 // The main is rewritten to allow for MPI initializing and for selecting a
 // reporter according to the process rank
