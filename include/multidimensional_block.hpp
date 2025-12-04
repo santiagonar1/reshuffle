d84b1893133c5fb846ba5a8bbd3da2c7200fba48
Santiago Narvaez <santiago.narvaez@tum.de>
2025-12-04 16:05:30 +0100

    feat: implement make_contiguous for MultidimensionalBlock

--- include/multidimensional_block.hpp
+++ include/multidimensional_block.hpp
@@ -5,6 +5,8 @@
 #include "coordinates.hpp"
 #include "profiler.hpp"
 
+#include <format>
+
 namespace reshuffle::internal {
     template<std::size_t N>
     using MultidimensionalBlock = std::array<Block, N>;
@@ -38,6 +40,65 @@ namespace reshuffle::internal {
             return sum + get_num_elements(block);
         });
     }
+
+    template<std::size_t N>
+    [[nodiscard]] auto replace_block(const MultidimensionalBlock<N> &multidimensional_block,
+                                     const Block &block, int dim) -> MultidimensionalBlock<N> {
+
+        if (dim < 0 or dim >= N) {
+            const auto err_msg = std::format("dim must be in [0, {}), but got {}", N, dim);
+            throw std::invalid_argument(err_msg);
+        }
+
+        auto new_multidimensional_block = multidimensional_block;
+        new_multidimensional_block[dim] = block;
+        return new_multidimensional_block;
+    }
+
+    template<std::size_t N>
+    [[nodiscard]] auto make_contiguous(const std::vector<MultidimensionalBlock<N>> &blocks, int dim)
+            -> std::vector<MultidimensionalBlock<N>> {
+
+        if (dim < 0 or dim >= N) {
+            const auto err_msg = std::format("dim must be in [0, {}), but got {}", N, dim);
+            throw std::invalid_argument(err_msg);
+        }
+
+        if (blocks.empty()) { return {}; }
+
+        const auto first_block = blocks.front();
+        const auto num_elements_first_block = first_block[dim].get_num_elements();
+
+        auto contiguous_blocks = std::vector<MultidimensionalBlock<N>>{};
+        contiguous_blocks.emplace_back(replace_block(
+                first_block, Block{{0, num_elements_first_block}, first_block[dim].get_owner()},
+                dim));
+
+        for (const auto &multidimensional_block: blocks | std::views::drop(1)) {
+            const auto last_block = contiguous_blocks.back();
+            const auto num_elements_current_block = multidimensional_block[dim].get_num_elements();
+            contiguous_blocks.emplace_back(
+                    replace_block(multidimensional_block,
+                                  Block{{last_block[dim].get_interval().get_right_bound(),
+                                         last_block[dim].get_interval().get_right_bound() +
+                                                 num_elements_current_block},
+                                        multidimensional_block[dim].get_owner()},
+                                  dim));
+        }
+
+        return contiguous_blocks;
+    }
+
+    template<std::size_t N>
+    [[nodiscard]] auto make_contiguous(const std::vector<MultidimensionalBlock<N>> &blocks)
+            -> std::vector<MultidimensionalBlock<N>> {
+        auto contiguous_blocks = blocks;
+        for (int dim = 0; dim < N; ++dim) {
+            contiguous_blocks = make_contiguous(contiguous_blocks, dim);
+        }
+        return contiguous_blocks;
+    }
+
 }// namespace reshuffle::internal
 
 #endif//MULTIDIMENSIONAL_BLOCK_HPP
--- tests/serial_tests/multidimensional_block_test.cpp
+++ tests/serial_tests/multidimensional_block_test.cpp
@@ -38,3 +38,46 @@ TEST(GetNumElements, CanBeUsedInVectorOfBlocks) {
     const auto expected = 2 * x_block.get_num_elements() * y_block.get_num_elements();
     EXPECT_THAT(get_num_elements(multi_blocks), Eq(expected));
 }
+
+TEST(ReplaceBlock, ChangesOneBlockOfMultidimensionalBlock) {
+    const auto block = Block{{0, 2}, 0};
+    const auto multiblock = MultidimensionalBlock<2>{block, block};
+
+    const auto new_block = Block{{1, 3}, 1};
+    const auto expected = MultidimensionalBlock{new_block, block};
+    EXPECT_THAT(replace_block(multiblock, new_block, 0), Eq(expected));
+}
+
+TEST(ReplaceBlock, ThrowsIfDimOutsideOfBounds) {
+    const auto block = Block{{0, 2}, 0};
+    const auto multiblock = MultidimensionalBlock<2>{block, block};
+
+    EXPECT_THROW(const auto _ = replace_block(multiblock, block, 2), std::invalid_argument);
+}
+
+TEST(MakeContiguous, MakesMultidimensionalBlocksContiguousInSpecifiedDimension) {
+    constexpr int dim = 0;
+    const auto first_multiblock = MultidimensionalBlock{Block{{0, 1}, 0}};
+    const auto second_multiblock = MultidimensionalBlock{Block{{3, 5}, 1}};
+    const auto multi_blocks = std::vector{first_multiblock, second_multiblock};
+
+    const auto expected = std::vector{MultidimensionalBlock{{Block{{0, 1}, 0}}},
+                                      MultidimensionalBlock{{Block{{1, 3}, 1}}}};
+    EXPECT_THAT(make_contiguous(multi_blocks, dim), Eq(expected));
+}
+
+TEST(MakeContiguous, ThrowsIfDimOutsideOfBounds) {
+    const auto multi_blocks = std::vector{MultidimensionalBlock{Block{{0, 1}, 0}}};
+    EXPECT_THROW(const auto _ = make_contiguous(multi_blocks, 2), std::invalid_argument);
+}
+
+TEST(MakeContiguous, IfNoDimensionaParameterGivenMakesBlocksContiguousInAllDimensions) {
+    const auto multi_blocks =
+            std::vector{MultidimensionalBlock{Block{{0, 1}, 0}, Block{{1, 2}, 1}},
+                        MultidimensionalBlock{Block{{3, 5}, 0}, Block{{1, 2}, 1}}};
+
+    const auto expected = std::vector{MultidimensionalBlock{Block{{0, 1}, 0}, Block{{0, 1}, 1}},
+                                      MultidimensionalBlock{Block{{1, 3}, 0}, Block{{1, 2}, 1}}};
+
+    EXPECT_THAT(make_contiguous(multi_blocks), Eq(expected));
+}
