41b175c854153b6663c1cc0c4c48828840f6d6af
Santiago Narvaez <santiago.narvaez@tum.de>
2025-12-08 14:23:58 +0100

    refactor: move comparison operator from BlockCyclic to DataDistribution
    Once I start using a DataDistribution in Context, I will need a
    comparison operator. So I might just move there to the base class
    already.

--- include/block_cyclic.hpp
+++ include/block_cyclic.hpp
@@ -42,8 +42,6 @@ namespace reshuffle {
         [[nodiscard]] auto get_num_blocks_per_dimension() const -> Dimensions<N>;
         [[nodiscard]] auto is_block_wise() const -> bool override;
 
-        [[nodiscard]] auto operator==(const BlockCyclic &other) const -> bool;
-
     private:
         const Dimensions<N> _num_global_values;
         const Dimensions<N> _block_sizes;
@@ -80,11 +78,6 @@ namespace reshuffle {
             }
         }
         return true;
-    }
-
-    template<std::size_t N>
-    auto BlockCyclic<N>::operator==(const BlockCyclic &other) const -> bool {
-        return _processor_grid == other._processor_grid and _grid_layout == other._grid_layout;
     }
 
     template<std::size_t N>
--- include/data_distribution.hpp
+++ include/data_distribution.hpp
@@ -14,7 +14,16 @@ namespace reshuffle {
         [[nodiscard]] virtual auto get_grid_layout() const -> const internal::GridLayout<N> & = 0;
         [[nodiscard]] virtual auto get_processor_grid() const -> const ProcessorGrid<N> & = 0;
         [[nodiscard]] virtual auto is_block_wise() const -> bool = 0;
+
+        [[nodiscard]] auto operator==(const DataDistribution &other) const -> bool;
     };
+
+    template<std::size_t N>
+    auto DataDistribution<N>::operator==(const DataDistribution &other) const -> bool {
+        return get_grid_layout() == other.get_grid_layout() and
+               get_processor_grid() == other.get_processor_grid();
+    }
+
 }// namespace reshuffle
 
 #endif//RESHUFFLE_DATA_DISTRIBUTION_HPP
