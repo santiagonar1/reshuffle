1828e6b8889914bc454d419f8550d28002d6c050
Santiago Narvaez <santiago.narvaez@tum.de>
2026-02-04 11:42:49 +0100

    feat(heat): implement operator<< matrix

--- demos/heat_transfer/include/matrix.hpp
+++ demos/heat_transfer/include/matrix.hpp
@@ -4,6 +4,8 @@
 
 namespace heat {
     using Matrix2D = std::vector<std::vector<double>>;
-}
+
+    auto operator<<(std::ostream &os, const Matrix2D &grid) -> std::ostream &;
+}// namespace heat
 
 #endif//RESHUFFLE_MATRIX_HPP
