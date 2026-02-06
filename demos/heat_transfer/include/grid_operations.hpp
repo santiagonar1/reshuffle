d518f6743d2b3a0476b183a2a5138b88f936dfca
Santiago Narvaez <santiago.narvaez@tum.de>
2026-02-06 12:59:19 +0100

    refactor(heat): add no discard to functions

--- demos/heat_transfer/include/grid_operations.hpp
+++ demos/heat_transfer/include/grid_operations.hpp
@@ -15,40 +15,45 @@ namespace heat {
     using OneDimensionRepresentation = std::pair<std::vector<double>, reshuffle::Dimensions<2>>;
 
     namespace internal {
-        auto set_boundary_to(const Matrix2D &grid, double value) -> Matrix2D;
+        [[nodiscard]] auto set_boundary_to(const Matrix2D &grid, double value) -> Matrix2D;
 
-        auto remove_top_row(const Matrix2D &grid) -> Matrix2D;
-        auto remove_bottom_row(const Matrix2D &grid) -> Matrix2D;
-        auto remove_left_column(const Matrix2D &grid) -> Matrix2D;
-        auto remove_right_column(const Matrix2D &grid) -> Matrix2D;
+        [[nodiscard]] auto remove_top_row(const Matrix2D &grid) -> Matrix2D;
+        [[nodiscard]] auto remove_bottom_row(const Matrix2D &grid) -> Matrix2D;
+        [[nodiscard]] auto remove_left_column(const Matrix2D &grid) -> Matrix2D;
+        [[nodiscard]] auto remove_right_column(const Matrix2D &grid) -> Matrix2D;
 
-        auto get_top_row(const Matrix2D &grid) -> std::vector<double>;
-        auto get_bottom_row(const Matrix2D &grid) -> std::vector<double>;
-        auto get_left_column(const Matrix2D &grid) -> std::vector<double>;
-        auto get_right_column(const Matrix2D &grid) -> std::vector<double>;
+        [[nodiscard]] auto get_top_row(const Matrix2D &grid) -> std::vector<double>;
+        [[nodiscard]] auto get_bottom_row(const Matrix2D &grid) -> std::vector<double>;
+        [[nodiscard]] auto get_left_column(const Matrix2D &grid) -> std::vector<double>;
+        [[nodiscard]] auto get_right_column(const Matrix2D &grid) -> std::vector<double>;
 
-        auto set_top_row(const Matrix2D &grid, const std::vector<double> &values)
+        [[nodiscard]] auto set_top_row(const Matrix2D &grid, const std::vector<double> &values)
                 -> std::expected<Matrix2D, SetBoundaryError>;
-        auto set_bottom_row(const Matrix2D &grid, const std::vector<double> &values)
+        [[nodiscard]] auto set_bottom_row(const Matrix2D &grid, const std::vector<double> &values)
                 -> std::expected<Matrix2D, SetBoundaryError>;
-        auto set_left_column(const Matrix2D &grid, const std::vector<double> &values)
+        [[nodiscard]] auto set_left_column(const Matrix2D &grid, const std::vector<double> &values)
                 -> std::expected<Matrix2D, SetBoundaryError>;
-        auto set_right_column(const Matrix2D &grid, const std::vector<double> &values)
+        [[nodiscard]] auto set_right_column(const Matrix2D &grid, const std::vector<double> &values)
                 -> std::expected<Matrix2D, SetBoundaryError>;
 
-        auto add_ghost_layers(const Matrix2D &grid) -> Matrix2D;
+        [[nodiscard]] auto add_ghost_layers(const Matrix2D &grid) -> Matrix2D;
     }// namespace internal
 
-    auto initialize_grid(unsigned int num_rows, unsigned int num_columns) -> Matrix2D;
-    auto to_grid(const OneDimensionRepresentation &input) -> std::expected<Matrix2D, ToGridError>;
+    [[nodiscard]] auto initialize_grid(unsigned int num_rows, unsigned int num_columns) -> Matrix2D;
+    [[nodiscard]] auto to_grid(const OneDimensionRepresentation &input)
+            -> std::expected<Matrix2D, ToGridError>;
 
-    auto get_dimensions(const Matrix2D &grid) -> std::pair<unsigned int, unsigned int>;
-    auto apply_jacobi(const Matrix2D &grid) -> Matrix2D;
+    [[nodiscard]] auto get_dimensions(const Matrix2D &grid)
+            -> std::pair<unsigned int, unsigned int>;
+    [[nodiscard]] auto apply_jacobi(const Matrix2D &grid) -> Matrix2D;
 
-    auto add_ghost_layers(const Matrix2D &grid, const ProcessorInfo &processor) -> Matrix2D;
-    auto get_ghost_layer(const Matrix2D &grid, const Location &location) -> std::vector<double>;
-    auto set_ghost_layer(const Matrix2D &grid, const std::vector<double> &values,
-                         const Location &location) -> std::expected<Matrix2D, SetBoundaryError>;
+    [[nodiscard]] auto add_ghost_layers(const Matrix2D &grid, const ProcessorInfo &processor)
+            -> Matrix2D;
+    [[nodiscard]] auto get_ghost_layer(const Matrix2D &grid, const Location &location)
+            -> std::vector<double>;
+    [[nodiscard]] auto set_ghost_layer(const Matrix2D &grid, const std::vector<double> &values,
+                                       const Location &location)
+            -> std::expected<Matrix2D, SetBoundaryError>;
 
 }// namespace heat
 
