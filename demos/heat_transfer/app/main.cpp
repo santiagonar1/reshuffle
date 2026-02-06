4e65395f130427e5893c530fa7fd5e377bf1c2f5
Santiago Narvaez <santiago.narvaez@tum.de>
2026-02-06 09:46:23 +0100

    feat(heat): add function to get processor grid

--- demos/heat_transfer/app/main.cpp
+++ demos/heat_transfer/app/main.cpp
@@ -1,12 +1,23 @@
-#include <iostream>
+#include "interval.hpp"
+#include "mpi_utils.hpp"
+#include "processor_grid.hpp"
+
+
+#include <mpi.h>
 
 #include <grid_operations.hpp>
 #include <vtk_writer.hpp>
 
-int main() {
+auto get_processor_grid() -> reshuffle::ProcessorGrid<2>;
+
+int main(int argc, char *argv[]) {
     constexpr auto num_iterations = 200;
     constexpr auto num_rows = 100;
     constexpr auto num_columns = num_rows;
+
+    MPI_Init(&argc, &argv);
+
+    const auto num_ranks = reshuffle::mpi::get_num_ranks(MPI_COMM_WORLD);
 
     const auto output_folder = std::filesystem::current_path() / "output";
     const auto files_prefix = "vtk_output_";
@@ -19,5 +30,16 @@ int main() {
         grid = heat::apply_jacobi(grid);
     }
 
+    MPI_Finalize();
     return 0;
+}
+
+auto get_processor_grid() -> reshuffle::ProcessorGrid<2> {
+    constexpr auto num_dimensions = 2;
+    const auto num_ranks = reshuffle::mpi::get_num_ranks(MPI_COMM_WORLD);
+
+    auto dimensions = reshuffle::Dimensions<num_dimensions>{};
+    MPI_Dims_create(num_ranks, num_dimensions, dimensions.data());
+
+    return reshuffle::ProcessorGrid{dimensions};
 }
