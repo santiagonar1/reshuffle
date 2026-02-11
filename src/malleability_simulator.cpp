88e1a367fe039d7199c293a72ab9becfe1f523ff
Santiago Narvaez <santiago.narvaez@tum.de>
2026-02-11 15:42:30 +0100

    feat: add a basic MalleabilitySimulator

---
+++ src/malleability_simulator.cpp
@@ -0,0 +1,58 @@
+#include "malleability_simulator.hpp"
+
+#include "mpi_utils.hpp"
+
+#include <assert.h>
+
+namespace reshuffle::mpi {
+    namespace internal {
+        [[nodiscard]] auto generate_rank_id_vector(unsigned int num_ranks) -> std::vector<RankId> {
+            return std::views::iota(0) | std::views::take(num_ranks) |
+                   std::ranges::to<std::vector<RankId>>();
+        }
+
+        [[nodiscard]] auto get_rank_status(const MPI_Comm &previous_comm, const MPI_Comm &new_comm)
+                -> RankStatus {
+            if (belongs_to_comm(previous_comm) and belongs_to_comm(new_comm)) {
+                return RankStatus::STAYING;
+            }
+
+            if (not belongs_to_comm(previous_comm) and belongs_to_comm(new_comm)) {
+                return RankStatus::JOINING;
+            }
+
+            if (belongs_to_comm(previous_comm) and not belongs_to_comm(new_comm)) {
+                return RankStatus::LEAVING;
+            }
+
+            if (not belongs_to_comm(previous_comm) and not belongs_to_comm(new_comm)) {
+                return RankStatus::INACTIVE;
+            }
+
+            assert(false && "[get_rank_status] Should not reach here");
+        }
+    }// namespace internal
+
+    MalleabilitySimulator::MalleabilitySimulator(const MPI_Comm &base_comm,
+                                                 const MPI_Comm &initial_comm)
+        : _base_comm{base_comm}, _current_comm{initial_comm} {}
+
+    auto MalleabilitySimulator::request_adaptation(const unsigned int num_ranks)
+            -> std::expected<std::pair<MPI_Comm &, RankStatus>, RequestAdaptationError> {
+        assert(num_ranks > 0 && "num_ranks must be greater than 0");
+
+        MPI_Barrier(_base_comm);
+
+        if (const auto available_ranks = get_num_ranks(_base_comm); num_ranks > available_ranks) {
+            return std::unexpected{RequestAdaptationError::NOT_ENOUGH_AVAILABLE_RANKS};
+        }
+
+        const auto new_comm =
+                get_sub_comm(_base_comm, internal::generate_rank_id_vector(num_ranks));
+        const auto rank_status = internal::get_rank_status(_current_comm, new_comm);
+
+        _current_comm = new_comm;
+        return std::pair<MPI_Comm &, RankStatus>{_current_comm, rank_status};
+    }
+
+}// namespace reshuffle::mpi
---
+++ tests/mpi_tests/malleability_simulator_test.cpp
@@ -0,0 +1,75 @@
+#include "malleability_simulator.hpp"
+#include "reshuffle.hpp"
+
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using namespace reshuffle::mpi;
+
+using testing::Eq;
+
+TEST(MalleabilitySimulator, NeedsABaseCommToBeCreated) {
+    const auto base_comm = MPI_COMM_WORLD;
+    const auto initial_comm = base_comm;
+
+    auto simulator = MalleabilitySimulator{base_comm, initial_comm};
+    const auto num_ranks = get_num_ranks(base_comm);
+
+    EXPECT_TRUE(simulator.request_adaptation(num_ranks).has_value());
+}
+
+TEST(RequestAdaptation, ReturnsRankStatus) {
+    const auto base_comm = MPI_COMM_WORLD;
+    const auto initial_comm = base_comm;
+
+    auto simulator = MalleabilitySimulator{base_comm, initial_comm};
+    const auto num_ranks = get_num_ranks(base_comm);
+
+    EXPECT_THAT(simulator.request_adaptation(num_ranks).value().second, Eq(RankStatus::STAYING));
+}
+
+TEST(RequestAdaptation, IndicatesIfRankIsJoining) {
+    const auto all_ranks_comm = MPI_COMM_WORLD;
+    const auto only_rank_0_comm = get_sub_comm(all_ranks_comm, std::vector{0});
+
+    auto simulator = MalleabilitySimulator{all_ranks_comm, only_rank_0_comm};
+    const auto num_ranks = get_num_ranks(all_ranks_comm);
+
+    const auto [_, rank_status] = simulator.request_adaptation(num_ranks).value();
+
+    if (is_root(all_ranks_comm)) {
+        EXPECT_THAT(rank_status, Eq(RankStatus::STAYING));
+    } else {
+        EXPECT_THAT(rank_status, Eq(RankStatus::JOINING));
+    }
+}
+
+TEST(RequestAdaptation, IndicatesIfRankIsLeaving) {
+    const auto all_ranks_comm = MPI_COMM_WORLD;
+
+    auto simulator = MalleabilitySimulator{all_ranks_comm, all_ranks_comm};
+
+    const auto [_, rank_status] = simulator.request_adaptation(1).value();
+
+    if (is_root(all_ranks_comm)) {
+        EXPECT_THAT(rank_status, Eq(RankStatus::STAYING));
+    } else {
+        EXPECT_THAT(rank_status, Eq(RankStatus::LEAVING));
+    }
+}
+
+TEST(RequestAdaptation, IndicatesIfRankIsInactive) {
+    const auto all_ranks_comm = MPI_COMM_WORLD;
+    const auto only_rank_0_comm = get_sub_comm(all_ranks_comm, std::vector{0});
+
+    auto simulator = MalleabilitySimulator{all_ranks_comm, only_rank_0_comm};
+
+    const auto [_, rank_status] = simulator.request_adaptation(1).value();
+
+    if (is_root(all_ranks_comm)) {
+        EXPECT_THAT(rank_status, Eq(RankStatus::STAYING));
+    } else {
+        EXPECT_THAT(rank_status, Eq(RankStatus::INACTIVE));
+    }
+}
