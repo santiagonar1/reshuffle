14a939b9c5bfff2dbf46611685ca04df317f3532
Santiago Narvaez <santiago.narvaez@tum.de>
2026-02-11 11:17:42 +0100

    feat(test): test more cases contexts comparison

--- tests/mpi_tests/context_test.cpp
+++ tests/mpi_tests/context_test.cpp
@@ -1,3 +1,6 @@
+#include "mpi_utils.hpp"
+
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
@@ -5,6 +8,7 @@
 #include <context.hpp>
 
 using namespace reshuffle;
+using namespace reshuffle::mpi;
 using namespace reshuffle::internal;
 
 TEST(Contexts, CanBeCompared) {
@@ -17,4 +21,32 @@ TEST(Contexts, CanBeCompared) {
             Context{BlockWise{Dimensions{num_global_values}, processor_grid}, MPI_COMM_WORLD};
 
     EXPECT_TRUE(context == same_context);
+}
+
+TEST(Context, TwoContextsWithDifferentCommsAreDifferent) {
+    constexpr auto num_global_values = 256;
+    const auto processor_grid = ProcessorGrid{3};
+
+    const auto comm_o = get_sub_comm(MPI_COMM_WORLD, std::vector{0});
+    const auto comm_1 = get_sub_comm(MPI_COMM_WORLD, std::vector{1});
+
+    const auto context = Context{BlockWise{Dimensions{num_global_values}, processor_grid}, comm_o};
+    const auto different_context =
+            Context{BlockWise{Dimensions{num_global_values}, processor_grid}, comm_1};
+
+    EXPECT_FALSE(context == different_context);
+}
+
+TEST(Context, TwoContextsWithDifferentDistributionsAreDifferent) {
+    constexpr auto num_global_values = 256;
+    const auto processor_grid = ProcessorGrid{3};
+
+    const auto comm_o = get_sub_comm(MPI_COMM_WORLD, std::vector{0});
+    const auto comm_1 = get_sub_comm(MPI_COMM_WORLD, std::vector{1});
+
+    const auto context = Context{BlockWise{Dimensions{num_global_values}, processor_grid}, comm_o};
+    const auto different_context =
+            Context{BlockWise{Dimensions{num_global_values + 1}, processor_grid}, comm_1};
+
+    EXPECT_FALSE(context == different_context);
 }
