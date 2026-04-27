e41fc4dae1e0bb7219cb5b6213f5b9665a971144
Santiago Narvaez <santiagonar1@gmail.com>
2026-04-27 17:41:50 +0200

    feat(build): update conan dependencies

--- conanfile.py
+++ conanfile.py
@@ -7,8 +7,8 @@ class MyProjectConan(ConanFile):
 
     def requirements(self):
         self.requires("gtest/1.17.0")
-        self.requires("abseil/20250814.0")
-        self.requires("benchmark/1.9.4")
+        self.requires("abseil/20260107.1")
+        self.requires("benchmark/1.9.5")
         self.requires("tracy/0.13.1")
         self.requires("zpp_bits/4.4.25")
 
